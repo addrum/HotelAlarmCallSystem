b38107cac660f6c7e3e9ed14d5bc4b9c5dce2243
Adam Short <adamshort1994@gmail.com>
2014-06-29 20:40:00 +0100

    upload including netbeans files

--- src/hotel/main/mkodo/HotelSystem.java
+++ src/hotel/main/mkodo/HotelSystem.java
@@ -11,100 +11,111 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
-public class HotelSystem implements Runnable {
+public class HotelSystem extends Thread {
 
-	private HashMap<Integer, String> callLog;
-	private SimpleDateFormat timeFormat = new SimpleDateFormat("kk:mm:ss");
-	private String wakeUpMessage = "This is your automatic wake up call for room number: ";
-	private String nextCall;
-	private Thread thread;
-	private String currentTime;
+    private HashMap<Integer, String> callLog;
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("kk:mm:ss");
+    private final String wakeUpMessage = "This is your automatic wake up call for room number(s): ";
+    private String nextCall;
+    private Thread thread;
+    private String currentTime;
+    private JTextArea viewPanel;
 
-	public HotelSystem() {
-		callLog = new LinkedHashMap<Integer, String>();
-		this.thread = new Thread(this);
-		this.thread.start();
-		setAlarmTime(4, "16:24:01");
-		setAlarmTime(5, "16:29:01");
-	}
+    public HotelSystem(JTextArea viewPanel) {
+        this.viewPanel = viewPanel;
+        callLog = new LinkedHashMap<Integer, String>();
+    }
 
-	@Override
-	public void run() {
-		while (true) {
-			currentTime = getCurrentTime();
-			if (callLog.containsValue(currentTime)) {
-				for (Entry<Integer, String> entry : callLog.entrySet()) {
-					if (entry.getValue().equals(currentTime)) {
-						sendAlarmCall(entry.getKey());
-						try {
-							Thread.sleep(300000);
-							System.out.println("Thread is sleeping");
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						System.out.println("Thread is awake");
-					}
-				}
-			}
-		}
-	}
+    @Override
+    public void run() {
+        while (true) {
+            currentTime = getCurrentTime();
+            if (callLog.containsValue(currentTime)) {
+                for (Entry<Integer, String> entry : callLog.entrySet()) {
+                    String numbers = "";
+                    if (entry.getValue().equals(currentTime)) {
+                        if (numbers.equals("")) {
+                            numbers += " + " + entry.getKey();
+                        } else {
+                            numbers = "" + entry.getKey();
+                        }
+                        sendAlarmCall(numbers);
+                        //sendAlarmCall(entry.getKey());
+                    }
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
-	// sends a generic alarm call message followed by the room number
-	public void sendAlarmCall(int roomNumber) {
-		System.out.println(wakeUpMessage + roomNumber);
-	}
+    @Override
+    public void start() {
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
 
-	// uses the iterator to get to first key:value pair in the hashmap
-	public String getNextCallRequest() {
-		Iterator it = callLog.entrySet().iterator();
-		if (it.hasNext()) {
-			Map.Entry pairs = (Map.Entry) it.next();
-			nextCall = "Room Number: " + pairs.getKey() + " - Time: " + pairs.getValue();
-			it.remove(); // avoids a ConcurrentModificationException
-			return nextCall;
-		} else {
-			return null;
-		}
-	}
+    // sends a generic alarm call message followed by the room number
+    private void sendAlarmCall(String numbers) {
+        viewPanel.setText(wakeUpMessage + numbers);
+        //callLog.remove(roomNumber);
+    }
 
-	public String getCurrentTime() {
-		Date time = Calendar.getInstance().getTime();
-		String currentTIme = timeFormat.format(time);
-		return currentTIme;
-	}
+    // uses the iterator to get to first key:value pair in the hashmap
+    public String getNextCallRequest() {
+        Iterator it = callLog.entrySet().iterator();
+        if (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry) it.next();
+            nextCall = "Room Number: " + pairs.getKey() + " - Time: " + pairs.getValue();
+            it.remove(); // avoids a ConcurrentModificationException
+            return nextCall;
+        } else {
+            nextCall = "null";
+            return nextCall;
+        }
+    }
 
-	public void setAlarmTime(int roomNumber, String time) {
-		Date alarmDate;
-		try {
-			alarmDate = timeFormat.parse(time);
-			String alarmTime = timeFormat.format(alarmDate);
-			callLog.put(roomNumber, alarmTime);
-			System.out.println("You entered room number: " + roomNumber + " and wake up time of: " + alarmTime);
-		} catch (ParseException e) {
-			System.out.println("Please ensure you seperate the time with colons eg 07:30:00.");
-		}
+    public String getCurrentTime() {
+        Date time = Calendar.getInstance().getTime();
+        String currentTIme = timeFormat.format(time);
+        return currentTIme;
+    }
 
-	}
+    public void setAlarmTime(int roomNumber, String time) {
+        try {
+            Date alarmDate = timeFormat.parse(time);
+            String alarmTime = timeFormat.format(alarmDate);
+            callLog.put(roomNumber, alarmTime);
+        } catch (ParseException e) {
+            viewPanel.setText("Please ensure you seperate the time with colons eg 07:30:00");
+        }
+    }
 
-	public void cancelAlarm(int roomNumber) {
-		if (callLog.containsKey(roomNumber)) {
-			callLog.remove(roomNumber);
-		}
-	}
+    public void cancelAlarm(int roomNumber) {
+        if (callLog.containsKey(roomNumber)) {
+            callLog.remove(roomNumber);
+            viewPanel.setText("Alarm cancelled for room number:" + roomNumber);
+        } else {
+            viewPanel.setText("No alarm exists for that room number.");
+        }
+    }
 
-	public void printHashMap() {
-		Iterator it = callLog.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry pairs = (Map.Entry) it.next();
-			nextCall = "Room Number: " + pairs.getKey() + " - Time: " + pairs.getValue();
-			System.out.println(nextCall);
-			it.remove(); // avoids a ConcurrentModificationException
-		}
-	}
-
-	public static void main(String[] args) {
-		new HotelSystem();
-	}
+    public void printHashMap() {
+        Iterator it = callLog.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry) it.next();
+            nextCall = "Room Number: " + pairs.getKey() + " - Time: " + pairs.getValue();
+            System.out.println(nextCall);
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+    }
 }
